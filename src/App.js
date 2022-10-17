5dcbbc9c40898ba02e13780a2726c470bdd0f79b
tidynoob <mitchellgriffin9@gmail.com>
2022-10-17 14:17:23 -0400

    Add sidebar header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {
   ChakraProvider,
   theme,
+  Grid,
+  Box
 } from '@chakra-ui/react';
+import Sidebar from './layout/Sidebar';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <Grid templateColumns='auto 1fr'>
+        <Sidebar />
+        <Box/>
+      </Grid>
+
     </ChakraProvider>
   );
 }
