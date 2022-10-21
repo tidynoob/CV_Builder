d161bf13bbba9577f9c0d6fede6a6cc2487b4559
tidynoob <mitchellgriffin9@gmail.com>
2022-10-21 10:33:29 -0700

    Update work experience

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,13 +1,18 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
 const PersonalInfo = (props) => {
     const personalInfo = props.personalInfo;
-    const {firstName, lastName, email, phone} = personalInfo;
+    const { firstName, lastName, email, phone } = personalInfo;
+
 
     return (
         <Box>
-            <Heading as='h3'>{firstName} {lastName}</Heading>
+            <Heading as='h2' size='xl'>{firstName} {lastName}</Heading>
             <Text><Text as="b">email:</Text> {email}</Text>
             <Text><Text as="b">phone:</Text> {phone}</Text>
         </Box>
@@ -15,14 +20,41 @@ const PersonalInfo = (props) => {
 
 }
 
+const WorkExperience = (props) => {
+    const workExperience = props.workExperience;
+
+    const elements = workExperience.map((item) => {
+        const { id, title, company, dateStart, dateEnd, description } = item;
+        // console.log(description);
+
+        if (!title) return null;
+        return (
+            <Box key={id}>
+                <Heading as='h3' size='lg'>{title}</Heading>
+                <Text as='b'>{company}</Text>
+                <Text>{monthNames[dateStart.getMonth()]} {dateStart.getFullYear()} - {monthNames[dateEnd.getMonth()]} {dateEnd.getFullYear()}</Text>
+                <Text className="description">{description}</Text>
+            </Box>
+        )
+    });
+
+    return (
+        <Box>
+            <Heading as='h2' size='xl'>Work Experience</Heading>
+            {elements}
+        </Box>
+    )
+}
+
+
 const Preview = (props) => {
-    const personalInfo = props.personalInfo;
-    // const workExperience = props.workExperience;
 
     return (
         <Box bg='white' color='gray.800' borderWidth='1px' width='full' height='full' borderRadius='lg' p='3'>
-            <PersonalInfo personalInfo={personalInfo}/>
+            <PersonalInfo personalInfo={props.personalInfo} />
             <Box mt='1' borderBottom='1px'></Box>
+            <WorkExperience workExperience={props.workExperience} />
+
         </Box>
     )
 };
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -5,42 +5,31 @@ import {
     Input,
     Grid,
     Button,
-    // useBoolean,
     GridItem,
     Textarea,
     Box,
     Flex,
     Text,
+    useColorModeValue
 } from "@chakra-ui/react";
 import { AddIcon } from '@chakra-ui/icons';
 import { useFormik } from "formik";
 import CollapseButton from "./CollapseButton";
 import uuid from "react-uuid";
+import { SingleDatepicker } from "chakra-dayzed-datepicker";
 
 const SubmittedForm = (props) => {
     const id = props.id;
     const workExperience = props.workExperience.filter((item) => item.id === id)[0];
-
     const submitted = workExperience.submitted;
     const title = workExperience.title;
     const company = workExperience.company;
 
-    const setWorkExperience = props.setWorkExperience;
-
     const handleEdit = () => {
-        setWorkExperience(props.workExperience.map((item) => {
-            if (item.id === id) {
-                return {
-                    ...item,
-                    submitted: false,
-                };
-            }
-            return item;
-        }));
+        props.handleEdit(id);
     }
-
     const handleDelete = () => {
-        setWorkExperience(props.workExperience.filter((item) => item.id !== id));
+        props.handleDelete(id);
     }
 
     return (
@@ -66,13 +55,42 @@ const WorkForm = (props) => {
     const submitted = workExperience.filter((item) => item.id === props.id)[0].submitted;
     const id = props.id;
 
+    const dateConfig = {
+        dateNavBtnProps: {
+            colorScheme: "teal",
+            variant: "ghost"
+        },
+        dayOfMonthBtnProps: {
+            defaultBtnProps: {
+                borderColor: "teal.500",
+                variant: "outline",
+                _hover: {
+                    background: useColorModeValue('teal.50', 'rgba(129, 230, 217, 0.12)')
+                },
+                _active: {
+                    background: useColorModeValue('teal.100', 'rgba(129, 230, 217, 0.24)')
+                }
+            },
+            selectedBtnProps: {
+                background: useColorModeValue('teal.100', 'rgba(129, 230, 217, 0.24)')
+                // color: "green",
+            },
+            todayBtnProps: {
+                borderColor: "teal.500",
+            }
+        },
+        inputProps: {
+            //   size: "sm"
+        }
+    };
+
     const formik = useFormik({
         initialValues: {
             title: '',
             company: '',
             description: '',
-            dateStart: '',
-            dateEnd: '',
+            dateStart: new Date(),
+            dateEnd: new Date(),
         },
         onSubmit: (values) => {
             setWorkExperience(workExperience.map((item) => {
@@ -95,20 +113,8 @@ const WorkForm = (props) => {
         formik.handleSubmit();
     }
 
-    const handleEdit = () => {
-        setWorkExperience(workExperience.map((item) => {
-            if (item.id === id) {
-                return {
-                    ...item,
-                    submitted: false,
-                };
-            }
-            return item;
-        }));
-    }
-
     const handleDelete = () => {
-        setWorkExperience(workExperience.filter((item) => item.id !== id));
+        props.handleDelete(id);
     }
 
     return (
@@ -125,11 +131,25 @@ const WorkForm = (props) => {
                     </FormControl>
                     <FormControl isDisabled={isDisabled}>
                         <FormLabel htmlFor="dateStart">Start Date</FormLabel>
-                        <Input onChange={formik.handleChange} value={formik.values.dateStart} type="date" id="dateStart" aria-describedby="dateStart-helper-text" />
+                        <SingleDatepicker
+                            name="dateStart"
+                            date={formik.values.dateStart}
+                            onDateChange={value => formik.setFieldValue('dateStart', value)}
+                            usePortal='true'
+                            propsConfigs={dateConfig}
+                            maxDate={new Date()}
+                        />
                     </FormControl>
                     <FormControl isDisabled={isDisabled}>
                         <FormLabel htmlFor="dateEnd">End Date</FormLabel>
-                        <Input onChange={formik.handleChange} value={formik.values.dateEnd} type="date" id="dateEnd" aria-describedby="dateEnd-helper-text" />
+                        <SingleDatepicker
+                            name="dateEnd"
+                            date={formik.values.dateEnd}
+                            onDateChange={value => formik.setFieldValue('dateEnd', value)}
+                            usePortal='true'
+                            propsConfigs={dateConfig}
+                            maxDate={new Date()}
+                        />
                     </FormControl>
                     <GridItem colSpan={2}>
                         <FormControl isDisabled={isDisabled}>
@@ -139,7 +159,6 @@ const WorkForm = (props) => {
                     </GridItem>
                 </Grid>
                 <Button isDisabled={isDisabled} type='submit' variant='solid' colorScheme='teal' ml='3'>Submit</Button>
-                <Button isDisabled={!isDisabled} onClick={handleEdit} variant='outline' colorScheme='teal' ml='3'>Edit</Button>
                 <Button onClick={handleDelete} variant='outline' colorScheme='red' ml='3'>Delete</Button>
 
             </form>
@@ -157,12 +176,28 @@ const WorkExperience = (props) => {
         setWorkExperience([...workExperience, { id: uuid(), submitted: false }]);
     }
 
+    const handleEdit = (id) => {
+        setWorkExperience(props.workExperience.map((item) => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    submitted: false,
+                };
+            }
+            return item;
+        }));
+    };
+
+    const handleDelete = (id) => {
+        setWorkExperience(props.workExperience.filter((item) => item.id !== id));
+    }
+
     const forms = (
         <Box>
             {workExperience.map((item) => (
                 <Box key={item.id}>
-                    <WorkForm setWorkExperience={setWorkExperience} workExperience={workExperience} id={item.id} />
-                    <SubmittedForm setWorkExperience={setWorkExperience} workExperience={workExperience} id={item.id} />
+                    <WorkForm setWorkExperience={setWorkExperience} workExperience={workExperience} id={item.id} handleDelete={handleDelete}/>
+                    <SubmittedForm setWorkExperience={setWorkExperience} workExperience={workExperience} id={item.id} handleEdit={handleEdit} handleDelete={handleDelete}/>
                 </Box>
             ))}
         </Box>
@@ -177,7 +212,7 @@ const WorkExperience = (props) => {
     )
 
     return (
-        <CollapseButton section='Work Experience' element={element} mt='6'/>
+        <CollapseButton section='Work Experience' element={element} mt='6' />
     );
 };
 
